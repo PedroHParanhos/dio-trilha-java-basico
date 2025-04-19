98bd0a716c6795eab37cd7a739b0e02234b72d74
Pedro H Paranhos <phenrique.paranhos@gmail.com>
2025-04-19 16:03:13 -0300

    desafio dio conta bancaria v1.0

--- Conta Banco/src/ContaTerminal.java
+++ Conta Banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Salve, Salve");
+        // TODO: Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o usuário
+
+        // Obter pela scanner os valores digitados no terminal
+
+        // Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o saldo da conta");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + " " + sobrenome
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta
+                + " e o seu saldo " + saldo + " já está disponível para saque!");
     }
 }
